28d05e32124556fce32ca09489305808c535f7d7
Andy86430 <155263725+Andy86430@users.noreply.github.com>
2024-01-01 18:58:03 +0000

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-# from modules.functions import get_PSratio
-# from modules.functions import convert_df
+from modules.functions import get_PSratio
+from modules.functions import convert_df
 from modules.functions import Zacks_Rank
 # from modules.functions import stock_price
 # from gspread_dataframe import set_with_dataframe
