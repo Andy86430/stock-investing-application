8ef060017ff49d4b769aec13777dbe7b62e81f9e
Andy86430 <155263725+Andy86430@users.noreply.github.com>
2024-01-02 14:09:46 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ import gspread
 from google.oauth2 import service_account
 
 # Connect to Google sheet
+global watchlist, portfolio
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"])
@@ -29,17 +30,19 @@ def run():
     upload = st.file_uploader('Upload "IBD Data Tables.xlsx"', type="xlsx")
     if upload is not None:
         if st.button('Produce Bullish List'):    
-            watchlist = pd.read_excel(upload).dropna()
-            watchlist = watchlist.rename(columns=watchlist.iloc[0]).drop(watchlist.index[0])
-            watchlist['Zack Rank'] = watchlist['Symbol'].apply(lambda x: Zacks_Rank(x))
-            watchlist = watchlist.loc[(watchlist['Zack Rank'].isin(['Buy', 'Strong Buy']))]
-            watchlist['PS'] = watchlist['Symbol'].apply(lambda x: get_PSratio(x))
-            watchlist = watchlist.sort_values(by=['PS'])
-            csv = convert_df(watchlist['Symbol'])
+            bullishlist = pd.read_excel(upload).dropna()
+            bullishlist = bullishlist.rename(columns=bullishlist.iloc[0]).drop(bullishlist.index[0])
+            bullishlist['Zack Rank'] = bullishlist['Symbol'].apply(lambda x: Zacks_Rank(x))
+            bullishlist = bullishlist.loc[(bullishlist['Zack Rank'].isin(['Buy', 'Strong Buy']))]
+            bullishlist['PS'] = bullishlist['Symbol'].apply(lambda x: get_PSratio(x))
+            bullishlist = bullishlist.sort_values(by=['PS'])
+            csv = convert_df(bullishlist['Symbol'])
             st.download_button("Download",csv,"Bullish List.csv","text/csv",key='download-csv')
 
     # Refresh stock prices
     if st.button('Refresh'):
+
+        global watchlist, portfolio
 
         # Refresh watchlist
         watchlist_df = pd.DataFrame.from_dict(watchlist.get_all_records())
