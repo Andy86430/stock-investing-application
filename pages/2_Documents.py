0376970b81bc647260751abd171340f7105ad7b0
Andy86430 <155263725+Andy86430@users.noreply.github.com>
2024-01-02 15:54:22 +0000

    update

--- pages/2_Documents.py
+++ pages/2_Documents.py
@@ -14,7 +14,7 @@ def display_csv(name):
 def documents() -> None:
 
     # Sidebar navigation
-    page_options = ["Confirmed_Uptrend", "Stock_Screening", "Breakout Setup", "Pullback Setup", "Bullish Candle Formations"]
+    page_options = ["Confirmed_Uptrend", "Stock_Screening", "Breakout_Setup", "Pullback_Setup"]
     selected_page = st.sidebar.selectbox("Documents", page_options)
 
     if selected_page == "Confirmed_Uptrend":
@@ -23,18 +23,11 @@ def documents() -> None:
     elif selected_page == "Stock_Screening":
         display_csv("Stock_Screening")
 
-    elif selected_page == "Breakout Setup":
-        display_csv("Breakout Setup")
+    elif selected_page == "Breakout_Setup":
+        display_csv("Breakout_Setup")
 
-    elif selected_page == "Pullback Setup":
-        display_csv("Pullback Setup")
-
-    elif selected_page == "Bullish Candle Formations":
-        st.image('hammer.jpeg', caption='Hammer')
-        st.image('Piercing.png', caption='Piercing')
-        st.image('Engulfing.png', caption='Piercing')
-        st.image('Morning Star.jpeg', caption='Piercing')
-
+    elif selected_page == "Pullback_Setup":
+        display_csv("Pullback_Setup")
 
 # Page config
 st.set_page_config(page_title="Documents", page_icon="book")
