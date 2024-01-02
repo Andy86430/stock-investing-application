3854dc819ccf7ec52c7073bd5434fd3bb529d9f1
Andy86430 <155263725+Andy86430@users.noreply.github.com>
2024-01-02 15:18:41 +0000

    update

--- pages/1_Portfolio.py
+++ pages/1_Portfolio.py
@@ -3,6 +3,8 @@ import pandas as pd
 from gspread_dataframe import set_with_dataframe
 from modules.functions import select_table
 from modules.config import jscode_buy_range
+from modules.functions import stock_price
+from modules.functions import Zacks_Rank
 
 def portfolio() -> None:
 
@@ -10,6 +12,21 @@ def portfolio() -> None:
     from streamlit_app import watchlist, portfolio
     portfolio_df = pd.DataFrame.from_dict(portfolio.get_all_records())
     watchlist_df = pd.DataFrame.from_dict(watchlist.get_all_records())
+
+    # Add a stock
+    cols = st.columns(3)
+    ticker = cols[0].text_input("Ticker:")
+    setup = cols[1].selectbox("Trading Setup:", ["Breakout", "Pullback"], index=0)
+    buy_point = cols[2].text_input("Buy Point:")
+    if st.button(label="Submit"):
+        new_row = pd.DataFrame(
+            {'Ticker': [ticker], 'Trading Setup': [setup], 'Buy Point': [buy_point], 'Price': [round(stock_price(ticker), 2)],
+             'Zacks Rank': [Zacks_Rank(ticker)]})
+        new_row['Buying Distance (%)'] = (100 * (new_row['Price'] / new_row['Buy Point'].astype(float) - 1)).round(1)
+        portfolio_df_updated = portfolio_df.append(new_row, ignore_index=True)
+        portfolio.clear()
+        set_with_dataframe(worksheet=portfolio, dataframe=portfolio_df_updated, include_index=False, include_column_header=True)
+        st.experimental_rerun()
 
     # Interactive table
     df_sel_row = select_table(portfolio_df.sort_values("Buying Distance (%)"), jscode_buy_range)
