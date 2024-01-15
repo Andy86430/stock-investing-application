d5d1455e0e5d1252022448b8f6aaaa41c6664ca6
Andy86430 <155263725+Andy86430@users.noreply.github.com>
2024-01-15 13:33:54 +0000

    update

--- pages/2_Documents.py
+++ pages/2_Documents.py
@@ -14,7 +14,8 @@ def display_csv(name):
 def documents() -> None:
 
     # Sidebar navigation
-    page_options = ["Mistakes to Avoid", "Investment Commandments", "Market Timing", "Confirmed Uptrend", "Stock Screener", "Trade Setup", "Climax Top", "Profit Taking"]
+    page_options = ["Mistakes to Avoid", "Investment Commandments", "Market Timing", "Confirmed Uptrend", 
+                    "Stock Screener", "Trade Setup", "Climax Top", "Profit Taking", "Indicator Categories"]
     selected_page = st.sidebar.selectbox("Documents", page_options)
 
     if selected_page == "Mistakes to Avoid":
@@ -44,6 +45,9 @@ def documents() -> None:
 
     elif selected_page == "Profit Taking":
         display_csv("Profit Taking")
+
+    elif selected_page == "Indicator Categories":
+        display_csv("Indicator Categories")
 
 # Page config
 st.set_page_config(page_title="Documents", page_icon="book")
