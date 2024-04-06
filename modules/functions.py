4db06e3d7af710c886a4c8dfaa4886868ccdc73f
Andy86430 <155263725+Andy86430@users.noreply.github.com>
2024-04-06 23:53:01 +0000

    u

--- modules/functions.py
+++ modules/functions.py
@@ -95,7 +95,7 @@ def highlight_cells(val):
         return ['background-color: green' if str(x).startswith(('1', '2')) else 'background-color: orange' if str(x).startswith(('3')) else 'background-color: orange' for x in val]
     elif val.name == 'Base Depth':
         return ['background-color: red' if float(x.strip('%')) > 30 else 'background-color: green' for x in val]
-    elif val.name == 'Breakout Vol% (Daily)':
+    elif val.name == 'Breakout Vol% (Daily)' or val.name == 'Breakout Vol% (Weekly)':
         return ['background-color: green' if float(x.strip('%')) > 40 else 'background-color: red' if float(x.strip('%')) < 0 else 'background-color: orange' for x in val]
     elif val.name == 'Handle Depth':
         return ['background-color: orange' if x=="N/A" else 'background-color: red' if float(x.strip('%')) > 15 else 'background-color: green' for x in val]
