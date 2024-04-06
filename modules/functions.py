6105395c8c403e3af457d907090c76db1e88087b
Andy86430 <155263725+Andy86430@users.noreply.github.com>
2024-04-06 21:41:46 +0000

    update

--- modules/functions.py
+++ modules/functions.py
@@ -92,6 +92,6 @@ def highlight_cells(val):
     elif val.name == 'Type of Base':
         return ['background-color: green' if x == "Cup with Handle" else 'background-color: orange' for x in val]
     elif val.name == 'Stage':
-        return ['background-color: green' if x.startswith(('1', '2')) else 'background-color: orange' if x.startswith(('3')) else 'background-color: orange' for x in val]
+        return ['background-color: green' if str(x).startswith(('1', '2')) else 'background-color: orange' if str(x).startswith(('3')) else 'background-color: orange' for x in val]
     else:
         return [''] * len(val)
