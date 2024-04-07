0536bc86f7c539fbae62e277bc042399c02d914e
Andy86430 <155263725+Andy86430@users.noreply.github.com>
2024-04-07 10:57:13 +0000

    update

--- modules/functions.py
+++ modules/functions.py
@@ -87,10 +87,10 @@ def highlight_cells(val):
         return ['background-color: green' if x == "At/Near New High" else 'background-color: red' for x in val]
     
     elif val.name == '%b':
-        return ['background-color: green' if x >0.8 else 'background-color: orange' for x in val]
+        return ['background-color: green' if x >= 0.8 else 'background-color: orange' for x in val]
     
     elif val.name == 'MFI (10)':
-        return ['background-color: green' if x >80 else 'background-color: orange' for x in val]
+        return ['background-color: green' if x >= 80 else 'background-color: orange' for x in val]
     
     elif val.name == 'Bollinger Squeeze' or val.name == 'Leaderboard':
         return ['background-color: green' if x == "Yes" else 'background-color: orange' for x in val]
@@ -105,7 +105,7 @@ def highlight_cells(val):
         return ['background-color: red' if float(x.strip('%')) > 30 else 'background-color: green' for x in val]
     
     elif val.name == 'Breakout Vol% (Daily)' or val.name == 'Breakout Vol% (Weekly)':
-        return ['background-color: green' if float(x.strip('%')) > 40 else 'background-color: red' if float(x.strip('%')) < 0 else 'background-color: orange' for x in val]
+        return ['background-color: green' if float(x.strip('%')) >= 40 else 'background-color: red' if float(x.strip('%')) < 0 else 'background-color: orange' for x in val]
     
     elif val.name == 'Handle Depth':
         return ['background-color: orange' if x=="N/A" else 'background-color: red' if float(x.strip('%')) > 15 else 'background-color: green' for x in val]
@@ -115,6 +115,9 @@ def highlight_cells(val):
     
     elif val.name == 'EPS Rating' or val.name == 'Comp Rating':
         return ['background-color: green' if x >= 80 else 'background-color: red' for x in val]
-    
+
+    elif val.name == 'RS Rating':
+        return ['background-color: green' if x >= 90 else 'background-color: red' if x < 80 else 'background-color: orange' for x in val]
+
     else:
         return [''] * len(val)
