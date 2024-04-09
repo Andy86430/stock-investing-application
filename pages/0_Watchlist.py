df6c8d12280e932228545b8beb018ede63c26136
Andy86430 <155263725+Andy86430@users.noreply.github.com>
2024-04-09 14:07:43 +0000

    u

--- pages/0_Watchlist.py
+++ pages/0_Watchlist.py
@@ -7,9 +7,7 @@ from modules.functions import stock_price
 from modules.functions import Zacks_Rank
 from modules.functions import highlight_cells
 from modules.functions import highlight_cells_ascending
-from streamlit import df_breakout
-from streamlit import df_pullback
-from streamlit import df_coil
+from streamlit_app import df_breakout, df_pullback, df_coil
 
 def display_csv(df):
 
